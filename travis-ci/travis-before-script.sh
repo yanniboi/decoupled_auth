83347b82407494e69a056b128e7216a712400332
yanniboi <yanniboi@1837556.no-reply.drupal.org>
2015-12-08 14:23:10 +0000

    Travis - Typo.

--- travis-ci/travis-before-script.sh
+++ travis-ci/travis-before-script.sh
@@ -7,12 +7,12 @@ set -e $DRUPAL_TI_DEBUG
 drupal_ti_ensure_drupal
 
 # Download module dependencies.
-(
+#(
 	# These variables come from environments/drupal-*.sh
 	#mkdir -p "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
     #cd "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
 	#git clone --branch 8.x-1.x http://git.drupal.org/project/module_we_need.git --depth 1
-)
+#)
 
 # Ensure the module is linked into the codebase.
 drupal_ti_ensure_module_linked
