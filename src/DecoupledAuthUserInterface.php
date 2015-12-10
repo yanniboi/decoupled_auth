e2bcd8f97f63273517f8de5366ec3988a2a7748f
andrewbelcher <andrewbelcher@655282.no-reply.drupal.org>
2015-12-10 06:35:12 -0500

    Issue #2631598 by andrewbelcher: Add isDecoupled/setDecoupled on the user class including an interface and renaming classes to make things clearer.

---
+++ src/DecoupledAuthUserInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\user\UserInterface.
+ */
+
+namespace Drupal\decoupled_auth;
+
+use Drupal\user\UserInterface;
+
+/**
+ * Provides an interface defining a user entity.
+ *
+ * @ingroup user_api
+ */
+interface DecoupledAuthUserInterface extends UserInterface {
+
+  /**
+   * Check whether this user is decoupled.
+   *
+   * @return bool
+   */
+  public function isDecoupled();
+
+  /**
+   * Set the decoupled state of this user.
+   *
+   * @param bool|NULL $decoupled
+   *   Whether the user is decoupled. If NULL, we work it out from whether name
+   *   is set. If FALSE, we clear out name/pass.
+   */
+  public function setDecoupled($decoupled = NULL);
+
+}
