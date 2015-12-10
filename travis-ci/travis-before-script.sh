6a9f47f4080c7a41eb3bcb638712798690bd4eb2
yanniboi <yanniboi@1837556.no-reply.drupal.org>
2015-12-10 12:16:08 +0000

    Issue #2631152 by yanniboi: Apply patch to core user module during testing so that any tests requiring the user module test with decoupled_auth enabled.

--- travis-ci/travis-before-script.sh
+++ travis-ci/travis-before-script.sh
@@ -6,21 +6,12 @@ set -e $DRUPAL_TI_DEBUG
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
 
-# Download module dependencies.
-#(
-	# These variables come from environments/drupal-*.sh
-	#mkdir -p "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
-    #cd "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
-	#git clone --branch 8.x-1.x http://git.drupal.org/project/module_we_need.git --depth 1
-#)
-
 # Ensure the module is linked into the codebase.
 drupal_ti_ensure_module_linked
 
-# Require that minimal profile enables decoupled_auth.
-ls $DRUPAL_TI_DRUPAL_DIR
-ls $DRUPAL_TI_DRUPAL_DIR/modules/decoupled_auth
-cp $DRUPAL_TI_DRUPAL_DIR/modules/decoupled_auth/travis-ci/minimal.info.yml $DRUPAL_TI_DRUPAL_DIR/core/profiles/minimal
+# Require that decoupled_auth is always enabled when the user module is enabled.
+cd $DRUPAL_TI_DRUPAL_DIR
+git apply -v $DRUPAL_TI_DRUPAL_DIR/modules/decoupled_auth/travis-ci/decoupled_auth_user_modules_installed.patch
 
 # Enable main module and submodules.
 drush en -y decoupled_auth
