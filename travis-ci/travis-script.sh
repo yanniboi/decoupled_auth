b1bce787e004f85c2e93abe570a8a45a8926bab5
yanniboi <yan@yanniboi.com>
2016-01-07 15:21:35 +0000

    By yanniboi: Devel on Travis.

--- travis-ci/travis-script.sh
+++ travis-ci/travis-script.sh
@@ -15,3 +15,13 @@ else
   echo 'Scheduling decoupled_auth phpunit tests to be run.\n\n'
   $DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpunit --group decoupled_auth
 fi
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  echo 'Scheduling all simpletests to be run.'
+  export DRUPAL_TI_SIMPLETEST_GROUP='decoupled_auth'
+  export $DRUPAL_TI_SIMPLETEST_GROUP='decoupled_auth'
+else
+  echo 'Scheduling decoupled_auth simpletests to be run.'
+  export DRUPAL_TI_SIMPLETEST_GROUP='decoupled_auth'
+  export $DRUPAL_TI_SIMPLETEST_GROUP='decoupled_auth'
+fi
