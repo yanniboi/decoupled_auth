15df7af56140a36efa21068158b725827cc136e7
yanniboi <yan@yanniboi.com>
2016-01-07 15:06:10 +0000

    By yanniboi: Devel on Travis.

--- travis-ci/travis-script.sh
+++ travis-ci/travis-script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-set -ev $DRUPAL_TI_DEBUG
+set -e $DRUPAL_TI_DEBUG
 
 # Run PHPUnit tests and submit code coverage statistics.
 drupal_ti_ensure_drupal
 drupal_ti_ensure_module_linked
 cd $DRUPAL_TI_DRUPAL_DIR/core
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  $DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpunit
-else
-  $DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpunit --group decoupled_auth
-fi
+#if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+#  $DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpunit
+#else
+#  $DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpunit --group decoupled_auth
+#fi
