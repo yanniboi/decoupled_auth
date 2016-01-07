88a3de129610e919eb0513dc63cf9e00d0d4d93b
yanniboi <yan@yanniboi.com>
2016-01-07 14:57:54 +0000

    By yanniboi: Devel on Travis.

--- travis-ci/travis-before-script.sh
+++ travis-ci/travis-before-script.sh
@@ -15,3 +15,13 @@ git apply -v $DRUPAL_TI_DRUPAL_DIR/modules/decoupled_auth/travis-ci/decoupled_au
 
 # Enable main module and submodules.
 drush en -y decoupled_auth
+
+
+if [ -n "$DRUPAL_TI_SIMPLETEST_GROUP" ]
+then
+  echo $DRUPAL_TI_SIMPLETEST_GROUP
+  export DRUPAL_TI_SIMPLETEST_GROUP='cheese'
+  echo $DRUPAL_TI_SIMPLETEST_GROUP
+  export $DRUPAL_TI_SIMPLETEST_GROUP='cheese'
+  echo $DRUPAL_TI_SIMPLETEST_GROUP
+fi
--- travis-ci/travis-script.sh
+++ travis-ci/travis-script.sh
@@ -7,17 +7,8 @@ drupal_ti_ensure_drupal
 drupal_ti_ensure_module_linked
 cd $DRUPAL_TI_DRUPAL_DIR/core
 
-#if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-#  $DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpunit
-#else
-#  $DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpunit --group decoupled_auth
-#fi
-
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  export ARGS=( $DRUPAL_TI_SIMPLETEST_ARGS )
-
-  cd "$DRUPAL_TI_DRUPAL_DIR"
-  { php "$DRUPAL_TI_SIMPLETEST_FILE" --php $(which php) "${ARGS[@]}" || echo "1 fails"; } | tee /tmp/simpletest-result.txt
-
-  egrep -i "([1-9]+ fail[s]?\s)|(Fatal error)|([1-9]+ exception[s]?\s)" /tmp/simpletest-result.txt && exit 1
+  $DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpunit
+else
+  $DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpunit --group decoupled_auth
 fi
