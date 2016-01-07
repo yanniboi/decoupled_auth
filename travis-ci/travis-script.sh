695c5e1b30d5a64be96eb0e14eb6fa3ede2b06ba
yanniboi <yan@yanniboi.com>
2016-01-07 14:00:58 +0000

    By yanniboi: Devel on Travis.

--- travis-ci/travis-script.sh
+++ travis-ci/travis-script.sh
@@ -7,5 +7,5 @@ drupal_ti_ensure_drupal
 drupal_ti_ensure_module_linked
 cd $DRUPAL_TI_DRUPAL_DIR/core
 
-#$DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpunit --group decoupled_auth
-$DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpunit
+$DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpunit --group decoupled_auth
+#$DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpunit
