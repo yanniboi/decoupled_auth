61cef42fd86ee50c3024b97413d3a76b32807635
yanniboi <yan@yanniboi.com>
2016-01-11 16:31:07 +0000

    Issue #2648240 by yanniboi: Added views filter and field handler for Decoupled property.

---
+++ src/Plugin/views/field/Decoupled.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\decoupled_auth\Plugin\views\field\Decoupled.
+ */
+
+namespace Drupal\decoupled_auth\Plugin\views\field;
+
+use Drupal\views\Plugin\views\display\DisplayPluginBase;
+use Drupal\views\ViewExecutable;
+use Drupal\views\Plugin\views\field\Boolean;
+use Drupal\views\ResultRow;
+
+/**
+ * Field handler to whether user is decoupled.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("user_decoupled")
+ */
+class Decoupled extends Boolean {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
+    parent::init($view, $display, $options);
+
+    $this->additional_fields['uid'] = 'uid';
+  }
+
+  public function query() {
+    $this->ensureMyTable();
+    $this->addAdditionalFields();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    $uid = $this->getValue($values, 'uid');
+    $user = \Drupal::entityTypeManager()->getStorage('user')->load($uid);
+
+    return $user->isDecoupled() ? 'Decoupled' : 'Coupled';
+  }
+
+}
