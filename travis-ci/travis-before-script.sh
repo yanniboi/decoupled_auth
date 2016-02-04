eac4760ffe4509925bfdd298d155967d1b6709eb
yanniboi <yanniboi@1837556.no-reply.drupal.org>
2016-02-04 10:36:27 +0000

    Issue #2653492 by yanniboi: Added profile and entity and composer_manager to travis.

--- travis-ci/travis-before-script.sh
+++ travis-ci/travis-before-script.sh
@@ -6,6 +6,15 @@ set -e $DRUPAL_TI_DEBUG
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
 
+# Download module dependencies.
+mkdir -p "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
+cd "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
+git clone --branch 8.x-1.x http://git.drupal.org/project/composer_manager.git --depth 1
+git clone --branch 8.x-1.x http://git.drupal.org/project/profile.git --depth 1
+git clone --branch 8.x-1.x http://git.drupal.org/project/entity.git --depth 1
+php composer_manager/scripts/init.php
+composer drupal-update
+
 # Ensure the module is linked into the codebase.
 drupal_ti_ensure_module_linked
 
