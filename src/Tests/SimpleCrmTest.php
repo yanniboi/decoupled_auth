5a824c0635c878742be3176755f50be443ca6c35
yanniboi <yanniboi@1837556.no-reply.drupal.org>
2016-02-05 12:17:59 +0000

    Issue #2653492 by yanniboi: Added photo field to individual profile and updated test.

--- src/Tests/SimpleCrmTest.php
+++ src/Tests/SimpleCrmTest.php
@@ -9,7 +9,8 @@ namespace Drupal\decoupled_auth\Tests;
 
 use Drupal\simpletest\WebTestBase;
 use Drupal\profile\Tests\ProfileTestTrait;
-use Drupal\decoupled_auth\Tests\DecoupledAuthUserCreationTrait;
+use Drupal\Core\File\FileSystem;
+
 /**
  * Tests CRM fields and views.
  *
@@ -27,14 +28,14 @@ class SimpleCrmTest extends WebTestBase {
    *
    * @var \Drupal\user\Entity\User
    */
-  protected $indiv_user;
+  protected $indivUser;
 
   /**
    * Testing admin user.
    *
    * @var \Drupal\user\Entity\User
    */
-  protected $admin_user;
+  protected $adminUser;
 
   /**
    * {@inheritdoc}
@@ -50,10 +51,10 @@ class SimpleCrmTest extends WebTestBase {
     ]);
 
     // Create a user with permissions for the crm_indiv profile type.
-    $this->indiv_user = $this->createUser();
-    $this->indiv_user->addRole('crm_indiv');
-    $this->indiv_user->save();
-    $this->admin_user = $this->createUser([], NULL, TRUE);
+    $this->indivUser = $this->createUser();
+    $this->indivUser->addRole('crm_indiv');
+    $this->indivUser->save();
+    $this->adminUser = $this->createUser([], NULL, TRUE);
 
   }
 
@@ -64,7 +65,7 @@ class SimpleCrmTest extends WebTestBase {
    *   An associative array of user information or FALSE if not users are found.
    */
   protected function getCoupledUsersFromView() {
-    $this->drupalLogin($this->admin_user);
+    $this->drupalLogin($this->adminUser);
 
     $this->drupalGet('admin/people');
     $this->assertResponse(200);
@@ -81,22 +82,23 @@ class SimpleCrmTest extends WebTestBase {
       }
 
       foreach($account->td[1]->a->attributes() as $label => $attribute) {
-        if ($label = 'href') {
+        if ($label == 'href') {
           $href = explode('/', $attribute);
           $uid = array_pop($href);
+          $this->verbose($attribute);
+          $this->verbose($uid);
         }
       }
 
-      $result_accounts[$name] = array(
-        'name' => $name,
-        'status' => (string) $account->td[2],
-        'roles' => $roles,
-        'member_for' => (string) $account->td[4],
-        'last_access' => (string) $account->td[5],
-      );
-
       if (isset($uid)) {
-        $result_accounts[$name]['uid'] = $uid;
+        $result_accounts[$uid] = array(
+          'name' => $name,
+          'status' => (string) $account->td[2],
+          'roles' => $roles,
+          'member_for' => (string) $account->td[4],
+          'last_access' => (string) $account->td[5],
+          'uid' => $uid,
+        );
       }
     }
 
@@ -110,7 +112,7 @@ class SimpleCrmTest extends WebTestBase {
    *   An associative array of user information or FALSE if not users are found.
    */
   protected function getDecoupledUsersFromView() {
-    $this->drupalLogin($this->admin_user);
+    $this->drupalLogin($this->adminUser);
 
     $this->drupalGet('admin/people/contacts');
     $this->assertResponse(200);
@@ -120,7 +122,6 @@ class SimpleCrmTest extends WebTestBase {
 
     foreach ($result as $account) {
 
-      $this->verbose(json_encode($account));
 
       $email = (string) $account->td[1];
       $email_components = explode('@', $email);
@@ -133,14 +134,24 @@ class SimpleCrmTest extends WebTestBase {
         }
       }
 
-      $result_accounts[$name] = array(
-        'name' => $name,
-        'email' => $email,
-        'roles' => $roles,
-      );
+      $this->verbose($account->td[5]);
+
+
+      foreach($account->td[5]->a->attributes() as $label => $attribute) {
+        if ($label == 'href') {
+          $href = substr($attribute, 0, strpos($attribute, '?') - 5);
+          $href = explode('/', $href);
+          $uid = array_pop($href);
+        }
+      }
 
       if (isset($uid)) {
-        $result_accounts[$name]['uid'] = $uid;
+        $result_accounts[$uid] = array(
+          'name' => $name,
+          'email' => $email,
+          'roles' => $roles,
+          'uid' => $uid,
+        );
       }
     }
     return !empty($result_accounts) ? $result_accounts : FALSE;
@@ -152,50 +163,55 @@ class SimpleCrmTest extends WebTestBase {
   public function testContactViews() {
     // Create a decoupled user to test views.
     $user_1 = $this->createDecoupledUser();
-    $email_prefix = $user_1->email_prefix;
 
     // Fetch the user accounts from both views.
     $coupled_accounts = $this->getCoupledUsersFromView();
     $decoupled_accounts = $this->getDecoupledUsersFromView();
 
     // Check decoupled user is in contacts view and not people view.
-    $this->assertFalse(in_array($email_prefix, array_keys($coupled_accounts)));
-    $this->assertTrue(in_array($email_prefix, array_keys($decoupled_accounts)));
+    $this->assertFalse(in_array($user_1->id(), array_keys($coupled_accounts)));
+    $this->assertTrue(in_array($user_1->id(), array_keys($decoupled_accounts)));
 
     // Check coupled users are in people view and not contacts view.
-    $this->assertTrue(in_array($this->indiv_user->label(), array_keys($coupled_accounts)));
-    $this->assertTrue(in_array($this->admin_user->label(), array_keys($coupled_accounts)));
-    $this->assertFalse(in_array($this->indiv_user->label(), array_keys($decoupled_accounts)));
-    $this->assertFalse(in_array($this->admin_user->label(), array_keys($decoupled_accounts)));
+    $this->assertTrue(in_array($this->indivUser->id(), array_keys($coupled_accounts)));
+    $this->assertTrue(in_array($this->adminUser->id(), array_keys($coupled_accounts)));
+    $this->assertFalse(in_array($this->indivUser->id(), array_keys($decoupled_accounts)));
+    $this->assertFalse(in_array($this->adminUser->id(), array_keys($decoupled_accounts)));
   }
 
   /**
    * Test Simple Crm fields.
    */
   public function testContactFields() {
-    $this->drupalLogin($this->indiv_user);
+    $this->drupalLogin($this->indivUser);
+
+    // Save a new picture.
+    $image = current($this->drupalGetTestFiles('image'));
+    $path = $this->container->get('file_service')->realpath($image->uri);
 
     // Submit the profile add form for an crm_indiv profile.
     $edit = [
-      'crm_name[0][value]' => $this->indiv_user->getDisplayName(),
-      'crm_email[0][value]' => $this->indiv_user->getEmail(),
+      'crm_name[0][value]' => $this->indivUser->getDisplayName(),
+      'crm_email[0][value]' => $this->indivUser->getEmail(),
       'crm_dob[0][value][year]' => '1990',
       'crm_dob[0][value][month]' => '1',
       'crm_dob[0][value][day]' => '1',
       'crm_gender' => 'female',
+      'files[field_crm_photo_0]' => $path,
     ];
-    $this->drupalPostForm('user/' . $this->indiv_user->id() . '/crm_indiv', $edit, t('Save'));
+    $this->drupalPostForm('user/' . $this->indivUser->id() . '/crm_indiv', $edit, t('Save'));
 
     // Check that the fields have been saved.
-    $this->drupalGet('user/' . $this->indiv_user->id() . '/crm_indiv');
+    $this->drupalGet('user/' . $this->indivUser->id() . '/crm_indiv');
     $this->assertResponse(200);
 
-    $this->assertFieldByName('crm_name[0][value]', $this->indiv_user->getDisplayName());
-    $this->assertFieldByName('crm_email[0][value]', $this->indiv_user->getEmail());
+    $this->assertFieldByName('crm_name[0][value]', $this->indivUser->getDisplayName());
+    $this->assertFieldByName('crm_email[0][value]', $this->indivUser->getEmail());
     $this->assertFieldByName('crm_dob[0][value][year]', '1990');
     $this->assertFieldByName('crm_dob[0][value][month]', '1');
     $this->assertFieldByName('crm_dob[0][value][day]', '1');
     $this->assertFieldByName('crm_gender', 'female');
+    $this->assertFieldByName('files[field_crm_photo_0]', $path);
   }
 
 }
