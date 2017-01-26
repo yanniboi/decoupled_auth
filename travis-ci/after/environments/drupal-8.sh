a35c402cca224dbb81485f8b592b5e516ed611ec
yanniboi <yanniboi@1837556.no-reply.drupal.org>
2017-01-26 13:17:53 +0000

    by yanniboi: Updated travis config.

---
+++ travis-ci/after/environments/drupal-8.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# @file
+# Drupal-8 environment variables and functions.
+
+#
+# Install drupal.
+#
+function drupal_ti_install_drupal() {
+    if [ "$TRAVIS_EVENT_TYPE" = "cron" ]
+    then
+        composer create-project drupal/drupal:8.*@dev -n
+    else
+        composer create-project drupal/drupal -n
+    fi
+
+    cd drupal
+    composer config extra.enable-patching true
+    composer config extra.merge-plugin.merge-extra true
+    composer require cweagans/composer-patches ~1.6
+    composer install
+    php -d sendmail_path=$(which true) ~/.composer/vendor/bin/drush.php --yes -v site-install "$DRUPAL_TI_INSTALL_PROFILE" --db-url="$DRUPAL_TI_DB_URL"
+    drush use $(pwd)#default
+}
+
+#
+# Ensures that the module is linked into the Drupal code base.
+#
+function drupal_ti_ensure_module_linked() {
+	# Ensure we are in the right directory.
+	cd "$DRUPAL_TI_DRUPAL_DIR"
+
+	# This function is re-entrant.
+	if [ -L "$DRUPAL_TI_MODULES_PATH/$DRUPAL_TI_MODULE_NAME" ]
+	then
+		return
+	fi
+
+	composer config "repositories.$DRUPAL_TI_MODULE_NAME" path $TRAVIS_BUILD_DIR
+	composer config repositories.drupal composer https://packages.drupal.org/8
+	composer require drupal/$DRUPAL_TI_MODULE_NAME *@dev
+
+	cat composer.json
+
+	rm -rf "$DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME"
+	# Find absolute path to module.
+	MODULE_DIR=$(cd "$TRAVIS_BUILD_DIR"; pwd)
+	# Point module into the drupal installation.
+	ln -sf "$MODULE_DIR" "$DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME"
+
+	ls -al $DRUPAL_TI_DRUPAL_DIR
+	ls -al $DRUPAL_TI_DRUPAL_DIR/modules
+	ls -al $DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME
+	ls -al $DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME/travis-ci
+
+	git apply -v $DRUPAL_TI_DRUPAL_DIR/modules/decoupled_auth/travis-ci/merging_data_types-2693081-15_0.patch
+
+	# Require that decoupled_auth is always enabled when the user module is enabled.
+	git apply -v $DRUPAL_TI_DRUPAL_DIR/modules/decoupled_auth/travis-ci/decoupled_auth_user_modules_installed.patch
+}
+
+#
+# Ensures that the module is linked but not installed.
+#
+function drupal_ti_ensure_module() {
+	# Ensure the module is linked into the code base.
+	drupal_ti_ensure_module_linked
+}
